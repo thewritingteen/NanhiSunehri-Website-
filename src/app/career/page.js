2e26a8501d437ff53cd1d45570e7221100c38612
thewritingteen <arpthunder@gmail.com>
2025-06-17 11:00:07 +0530

    Create page.js

---
+++ src/app/career/page.js
@@ -0,0 +1,12 @@
+// app/career/page.js (or pages/career.js)
+
+import CareerSection from "./components/CareerSection";
+// (or "../components/CareerSection" if using Pages Router and placing it generally)
+
+export default function CareerPage() {
+  return (
+    <>
+      <CareerSection />
+    </>
+  );
+}
