c70f2d92a1b2995492910c071be643cec5613205
thewritingteen <arpthunder@gmail.com>
2025-06-17 08:56:23 +0530

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,1 +1,19 @@
+// app/about/page.js
 
+import AboutHeroSection from "./components/AboutHeroSection";
+import MissionVisionSection from "./components/MissionVisionSection";
+import ValuesSection from "./components/ValuesSection";
+// import TeamSection from "./components/TeamSection"; // Uncomment if you add a team section
+import CtaSection from "@/components/landing/cta-section"; // Reusing your existing CTA
+
+export default function AboutPage() {
+  return (
+    <>
+      <AboutHeroSection />
+      <MissionVisionSection />
+      <ValuesSection />
+      {/* <TeamSection /> */} {/* Uncomment if you want to include a team section */}
+      <CtaSection /> {/* You can reuse the existing CtaSection from your landing components */}
+    </>
+  );
+}
